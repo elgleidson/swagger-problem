6bdef454e097d0ce456ec1150840a464b857a513
Gleidson Leopoldo <gleidson.leopoldo@ii.co.uk>
2020-04-16 08:08:36 +0100

    Migration to a normal spring-boot-web instead of webflux

--- src/main/java/com/github/elgleidson/swagger/problem/ApiController.java
+++ src/main/java/com/github/elgleidson/swagger/problem/ApiController.java
@@ -4,15 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
   @GetMapping
-  public Mono<ResponseEntity<MyResponse>> get() {
-    return Mono.just(ResponseEntity.ok(new MyResponse()));
+  public ResponseEntity<MyResponse> get() {
+    return ResponseEntity.ok(new MyResponse());
   }
 
 }

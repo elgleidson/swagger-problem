288219e1cbc6e007b5aeea65a98faaf6d0e9d0f8
Gleidson Leopoldo <el.gleidson@gmail.com>
2024-04-09 22:11:45 +0100

    Upgraded to java 21 and new versions of Spring Boot and libraries

--- src/test/java/com/github/elgleidson/swagger/problem/MyResponse.java
+++ src/test/java/com/github/elgleidson/swagger/problem/MyResponse.java
@@ -4,9 +4,9 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Value;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 @Value
